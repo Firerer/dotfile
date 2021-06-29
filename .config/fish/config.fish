7f2a157c70922549a473b330cfb62c1577fd8253
Di Liu <liudi12631@gmail.com>
2021-06-29 15:08:39 +1000

    rename config to dotconfig

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,4 @@
 alias sysinfo="neofetch"
 alias audio="pavucontrol"
 alias wifi="nmcli device wifi"
-alias config='/usr/bin/git --git-dir=/home/firer/.cfg/ --work-tree=/home/firer'
+alias dotconfig='/usr/bin/git --git-dir=/home/firer/.cfg/ --work-tree=/home/firer'
