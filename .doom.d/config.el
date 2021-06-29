4ac36a006e4bf65080ee661fda1bfa5141a45085
Di Liu <liudi12631@gmail.com>
2021-06-29 14:31:37 +1000

    init doom emacs config

---
+++ .doom.d/config.el
@@ -0,0 +1,39 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Di Liu"
+      user-mail-address "liudi12631@email.com")
+
+;; ui
+(setq doom-theme 'doom-one)
+(setq display-line-numbers-type 'relative)
+;font
+(setq doom-font (font-spec :family "mononoki" :size 20 :weight 'semi-light)
+      doom-big-font (font-spec :family "mononoki")
+)
+
+;; dirs
+(setq org-directory "~/org/")
+(setq projectile-project-search-path "~/Documents/codes/")
+(setq racer-rust-src-path (concat (getenv "HOME") "/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library"))
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
