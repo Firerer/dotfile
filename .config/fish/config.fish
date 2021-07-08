b4f7b44096a809038940425163395d08cf5eecfd
Di Liu <liudi12631@gmail.com>
2021-07-08 17:53:10 +0000

    update fish variables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,9 @@
+
+# sys utils
 alias sysinfo="neofetch"
 alias audio="pavucontrol"
 alias wifi="nmcli device wifi"
+alias diskinfo="df -h"
+alias memoinfo="free -m"
+
 alias dotconfig='/usr/bin/git --git-dir=/home/firer/.cfg/ --work-tree=/home/firer'
